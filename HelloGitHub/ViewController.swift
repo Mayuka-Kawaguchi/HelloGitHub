4ff75e80235e7237af698134123624ee73292113
K Mayuka <mayuka@MayukanoMacBook-puro.local>
2019-10-19 16:13:47 +0900

    first commit

---
+++ HelloGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloGitHub
+//
+//  Created by K Mayuka on 2019/10/19.
+//  Copyright © 2019 K Mayuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
